ddfd5b3c2a90066bef0e08e37f269f48ac4f057c
alvarocb1 <110578414+alvarocb1@users.noreply.github.com>
2023-06-15 21:25:39 -0400

    first commit

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const apiClient = axios.create({
   baseURL: 'https://api-nba-v1.p.rapidapi.com',
   headers: {
-    'x-rapidapi-key': '6e8c1f89a2msh9023223c3ba1345p19f59djsn37a77f7e743a',
+    'x-rapidapi-key': '',
     'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
   },
 });
