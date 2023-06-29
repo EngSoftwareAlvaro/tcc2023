8c51da5300f95725718e608e3375f708cd34c37e
alvarocb1 <802.291@alunos.unigran.br>
2023-06-28 21:35:13 -0400

    tela jogador v1

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 
